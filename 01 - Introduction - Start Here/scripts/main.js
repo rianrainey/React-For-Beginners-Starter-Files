1235c17b754bb8b27fdf9996105634f385d840e2
Rian Rainey <hello@rian.me>
2015-11-05 12:41:20 -0600

    Through Video 05 - Creating Our Application Layout w/ Components

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -2,6 +2,60 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 /*
+ * App
+ */
+
+var App = React.createClass({
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order/>
+        <Inventory/>
+      </div>
+    )
+  }
+});
+
+/*
+ * Header
+ */
+
+var Header = React.createClass({
+  render : function() {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+/*
+ * Order
+ */
+
+var Order = React.createClass({
+  render : function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
+ * Inventory
+ */
+
+var Inventory = React.createClass({
+  render : function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+/*
  * StorePicker
  * This will let us make <StorePicker/>
  */
@@ -18,4 +72,4 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
