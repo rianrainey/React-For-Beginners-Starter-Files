e9a7df08eda2965c81a9e0e32278913eff8d6fbf
Rian Rainey <hello@rian.me>
2015-11-05 11:23:28 -0600

    Through video 03 - Writing Your First Component

---
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+ * StorePicker
+ * This will let us make <StorePicker/>
+ */
+var StorePicker = React.createClass({
+  render : function() {
+    return (
+      <p>hi</p>
+    )
+  }
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
